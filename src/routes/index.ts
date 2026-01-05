5b70a75eff6ce0cf31a3f3cd1fc30e3f57813315
Zakaria Sobahi <sobahi.zakaria@gmail.com>
2026-01-05 17:00:21 +0100

    ibitialisation de docker

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,11 @@
 import { Router } from 'express';
 import { ParcelController } from '../controllers/parcel.controller';
+import { ZoneController } from '../controllers/zone.controller';
 
 const router = Router();
+
 router.post('/parcels/:id/assign', ParcelController.assign);
+router.get('/zones', ZoneController.list);
+router.post('/zones', ZoneController.create);
 
 export default router;
